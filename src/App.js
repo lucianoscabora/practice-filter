fc49974ee8ca75658955f02a9d7d1f3d328a5607
Luciano Scabora <lucianoscabora@gmail.com>
2023-01-25 19:35:38 -0300

    api practice frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchInput from "./components/SearchInput";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = 'https://kitsu.io/api/edge/';
+
+export default function App () {
+
+    // o retorno da APi deve ser guardada dentro de um state
+    const [info, setInfo] = useState({});
+    const [text, setText] = useState('');
+    
+    useEffect(() => {
+        if (text) {
+            setInfo({});
+            fetch(`${api}anime?filter[text]=${text}&page[limit]=12`)
+            .then((response) => response.json())
+            .then((response) => {
+                setInfo(response);
+                
+            })
+        }
+    }, [text]);
+
+    return (
+        <div>
+            <SearchInput value={text} onChange={(search) => setText(search)} />
+            {text && !info.data && (
+                        <span>Carregando...</span>
+                    )}
+            {info.data && (
+                // fazer um map para converter no JSX e apresentar na tela
+                <ul className="animes-list">
+                    {info.data.map((item) => (
+                        <li key={item.id}>
+                            <img src={item.attributes.posterImage.small} alt="each anime images" />
+                            {item.attributes.canonicalTitle}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
-
-export default App;
